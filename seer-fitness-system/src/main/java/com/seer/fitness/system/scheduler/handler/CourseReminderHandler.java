c827ab4b8a4b340aba8cbeffa653a079dce48907
＂mo <mocanjie@qq.com>
2026-03-14 19:11:41 +0800

    feat(scheduler): add JobHandler interface and CourseReminderHandler example

---
+++ seer-fitness-system/src/main/java/com/seer/fitness/system/scheduler/handler/CourseReminderHandler.java
@@ -0,0 +1,20 @@
+package com.seer.fitness.system.scheduler.handler;
+
+import com.seer.fitness.system.scheduler.JobHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 示例：课程提醒处理器
+ * handler_name = courseReminderHandler
+ */
+@Slf4j
+@Component("courseReminderHandler")
+public class CourseReminderHandler implements JobHandler {
+
+    @Override
+    public void execute(String params) throws Exception {
+        log.info("执行课程提醒任务, params={}", params);
+        // TODO: 实现课程提醒业务逻辑
+    }
+}
