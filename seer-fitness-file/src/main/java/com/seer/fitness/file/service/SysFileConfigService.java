05018062e73d036e96e3bf3a0bfaf3b30f87b076
＂mo <mocanjie@qq.com>
2026-03-15 20:45:58 +0800

    fix: @Lazy 打破 FileStorageManager 与 SysFileConfigService 循环依赖

--- seer-fitness-file/src/main/java/com/seer/fitness/file/service/SysFileConfigService.java
+++ seer-fitness-file/src/main/java/com/seer/fitness/file/service/SysFileConfigService.java
@@ -9,6 +9,7 @@ import io.github.canjiemo.base.myjdbc.service.impl.BaseServiceImpl;
 import io.github.canjiemo.mycommon.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import java.util.List;
 @Service
 public class SysFileConfigService extends BaseServiceImpl implements ISysFileConfigService {
 
+    @Lazy
     @Autowired
     private FileStorageManager fileStorageManager;
 
